6eb3d1653471b64b424ad6f3f160bc644549bead
ysng_ysng <yeasung67@gmail.com>
2023-10-14 21:09:34 +0900

    '도메인 주도개발 시작하기' 시작

---
+++ src/main/java/yosongsong/ddd/DomainDrivenDevelopmentStartApplication.java
@@ -0,0 +1,13 @@
+package yosongsong.ddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDrivenDevelopmentStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainDrivenDevelopmentStartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yosongsong/ddd/DomainDrivenDevelopmentStartApplicationTests.java
@@ -0,0 +1,13 @@
+package yosongsong.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainDrivenDevelopmentStartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
